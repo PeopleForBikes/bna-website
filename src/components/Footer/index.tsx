d2f3b0c200321966acab39bd16eb3772f4c7cd66
roberto <61357564+robahtou@users.noreply.github.com>
2023-04-23 15:04:02 -0500

    add placeholder components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      I'm a footer
+    </footer>
+  );
+}
+
+
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      HEADER
+    </header>
+  );
+}
+
+
+export default Header;
