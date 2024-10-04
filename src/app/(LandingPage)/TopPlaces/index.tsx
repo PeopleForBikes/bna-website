9a022569b6abc5b27aa2f3701df601662e5b2f9d
robahtou <61357564+robahtou@users.noreply.github.com>
2024-10-04 11:16:30 -0500

    populate top 10 places

--- src/app/(LandingPage)/TopPlaces/index.tsx
+++ src/app/(LandingPage)/TopPlaces/index.tsx
@@ -1,21 +1,73 @@
 import Link from 'next/link';
 
+import sql from '@Utils/postgres';
+
 import Icon from '@Icons';
 import styles from './styles.module.css';
 
 
-function TopPlaces() {
+async function getTopPlaces() {
+  const data = await sql`
+    WITH latest_scores AS (
+      SELECT
+        s.city_id,
+        s.score,
+        s.created_at,
+        ROW_NUMBER() OVER (
+          PARTITION BY s.city_id ORDER BY s.created_at DESC
+        ) AS rn
+      FROM
+        public.summary AS s
+    )
+    SELECT
+      c.name AS city_name,
+      c.state_abbrev,
+      c.country,
+      ls.score
+    FROM
+      latest_scores AS ls
+      JOIN public.city AS c
+        ON c.id = ls.city_id
+    WHERE
+      ls.rn = 1
+    ORDER BY
+      ls.score DESC
+    LIMIT 10;
+  `;
+
+  return data;
+}
+
+async function TopPlaces() {
+  const topPlaces = await getTopPlaces();
+  const topFivePlaces = topPlaces.slice(0, 5);
+  const bottomFivePlaces = topPlaces.slice(5, 10);
+
   return (
     <section className={styles['layout']}>
-      <h3 className={styles['title']}>Top Places</h3>
+      <h3 className={styles['title']}>Top 10 Places</h3>
 
-      <ul className={styles['']}>
-        <li className={styles['']}></li>
+      <ul className={styles['list']}>
+        {topFivePlaces.map((place) => (
+          <li className={styles['item']} key={place.city_name}>
+            <h3>{place.city_name}</h3>
+            <span>{`${place.state_abbrev}, ${place.country}`}</span>
+            <h1>{place.score}</h1>
+          </li>
+        ))}
+      </ul>
+      <ul className={styles['list']}>
+        {bottomFivePlaces.map((place) => (
+          <li className={styles['item']} key={place.city_name}>
+            <h3>{place.city_name}</h3>
+            <span>{`${place.state_abbrev}, ${place.country}`}</span>
+            <h1>{place.score}</h1>
+          </li>
+        ))}
       </ul>
 
-      <Link className={`btn-secondary ${styles['btn-link']}`} href="/location">
-        More places
-
+      <Link className={`btn-secondary ${styles['btn-link']}`} href="https://bna.peopleforbikes.org/#/places////">
+        More Places
         <Icon name="arrow-right" className={styles['icon-arrow-right']} />
       </Link>
     </section>
