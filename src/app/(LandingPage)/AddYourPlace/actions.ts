73f33c280b2895b5b1a0bc69201b97ed58009498
Roberto Fuentes <61357564+robahtou@users.noreply.github.com>
2024-10-18 10:59:23 -0500

    Hotfix (#28)
    * initial pass
    
    * bump packages
    
    * init design to dev transition; remove tailwind; update packages
    
    * init design to dev patterns
    
    * prototype(svgs): update how SVGs are to be used
    
    * Inline SVGs at the top of the page and reference them throughout.
    * update components to use new SVG method
    * keep all .svg files together
    * WIP
    
    * all-in-one
    
    * update packages
    
    * update packages
    
    * latest updates
    
    * remove yaml file
    
    * fix duplicate code from merge

--- src/app/(LandingPage)/AddYourPlace/actions.ts
+++ src/app/(LandingPage)/AddYourPlace/actions.ts
@@ -14,41 +14,6 @@ type Result = {
   } | null;
   data?: unknown;
 };
-interface AddYourPlaceFormData {
-  firstName: string;
-  lastName: string;
-  occupation: string | null;
-  organization: string | null;
-  email: string;
-  country: string;
-  city: string;
-  region: string | null;
-  fipsCode: string;
-  consent: boolean;
-}
-
-const requestToAddYourPlace = async (formData: AddYourPlaceFormData): Promise<void | { errors: boolean }> => {
-  try {
-    await sql`
-      INSERT INTO "public"."submission"
-        (first_name, last_name, occupation, organization, email, country, city, region, fips_code, consent, status)
-      VALUES
-        (${formData.firstName}, ${formData.lastName}, ${formData.occupation || null}, ${formData.organization || null}, ${formData.email}, ${formData.country}, ${formData.city}, ${formData.region || null }, ${formData.fipsCode}, ${formData.consent}, 'Pending');
-    `;
-  } catch (error) {
-    console.error('ServerAction:SQLerror\n', error);
-    return { errors: true };
-  }
-}
-
-
-async function addYourPlaceAction(prevState: Result, formData: AddYourPlaceFormData): Promise<Result> {
-  console.group('ServerAction -- addYourPlace');
-  console.info({ formData });
-
-  const validationErrors: Record<string, { message: string }> = {};
-  const validation = addYourPlaceSchema.safeParse(formData);
-  console.log({ validation });
 
 type ActionState = { success: boolean; errors: any; data: any };
 
@@ -76,7 +41,7 @@ async function addYourPlaceAction(
   const validationErrors: Record<string, { message: string }> = {};
   const validation = addYourPlaceSchema.safeParse(formData);
   console.log({ validation });
-    
+
   if(!validation.success) {
     for (const issue of validation.error.issues) {
       const { path, message } = issue;
@@ -94,6 +59,7 @@ async function addYourPlaceAction(
   }
 
   await requestToAddYourPlace(validation.data as AddYourPlaceFormData);
+
   return { success: true, errors: null, data: null };
 }
 
@@ -102,7 +68,7 @@ async function getCountries(): Promise<string[] | []> {
     const data = await sql`
       SELECT array_agg(name) AS country_names FROM "public"."country";
   `;
-    
+
     return data[0].country_names;
   } catch (error) {
     console.error('ServerAction:SQLerror\n', error);
